1d893440eb4ad0beddd79f6040ce7f7d610e3806
desan <desanctisisabella95@gmail.com>
2025-03-18 16:54:19 +0100

    commit eredit2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args){
+
+        Forma f = new Forma();
+        //double base;
+        //double altezza;
+        Rettangolo ret = new Rettangolo(10.0,20.8);
+        Triangolo tri = new Triangolo(12.5, 3.4);
+        f.calcolaArea();
+        ret.calcolaArea();
+        tri.calcolaArea();
     }
 }
